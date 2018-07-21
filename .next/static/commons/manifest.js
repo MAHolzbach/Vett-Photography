cf109496cce0a69ceca76f2daea812974642592d
Mike Holzbach <maholzbach@gmail.com>
2018-07-21 13:32:18 -0400

    Created landing page

--- .next/static/commons/manifest.js
+++ .next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "541c617c67e5c5a7aa34"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "4af7ae42e47715b28b01"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_error.js","2":"bundles\\pages\\_app.js"}[chunkId]||chunkId) + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_error.js","2":"bundles\\pages\\_app.js","4":"bundles\\pages\\index.js"}[chunkId]||chunkId) + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+
+export default () => (
+  <div className="landing">
+    <h1 className="header">Vett Photography</h1>;
+    <Link className="next-link" href="/portfolio">
+      <a className="enter-link">ENTER</a>
+    </Link>
+    <style jsx>{`
+      @import url("https://fonts.googleapis.com/css?family=Titillium+Web:300,400,700i");
+
+      .landing {
+        background-image: url("/static/lucas-landing.jpg");
+        height: 100vh;
+        margin: 0;
+        background-size: cover;
+        background-position: center;
+      }
+      .header {
+        margin: 0;
+        font-family: "Titillium Web", sans-serif;
+        color: white;
+        letter-spacing: 0.15em;
+        font-weight: 400;
+        font-size: 75px;
+        padding-top: 200px;
+        padding-right: 20px;
+        text-align: right;
+      }
+      .enter-link {
+        font-family: "Titillium Web", sans-serif;
+        float: right;
+        color: white;
+        padding-right: 40px;
+        font-size: 30px;
+        letter-spacing: 0.1em;
+        text-decoration: none;
+      }
+    `}</style>
+    <style jsx global>{`
+      html,
+      body {
+        margin: 0;
+      }
+    `}</style>
+  </div>
+);
